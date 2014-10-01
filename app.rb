d77df84e193819ddc924d693ceb9ecf9cd7f3315
Varun Murali <varunm@andrew.cmu.edu>
2014-09-30 23:39:57 -0400

    made environment variable and removed key from file

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sports_data_api'
 
 get '/' do
   @word = "Hello, World!"
-  SportsDataApi.set_key(:nfl, 'dsvqbre5qxsqkp5aemgtpgt2')
+  SportsDataApi.set_key(:nfl, ENV['NFLKEY'])
   SportsDataApi.set_access_level(:nfl, 't')
   @teams = SportsDataApi::Nfl.teams
   @all_teams = @teams.map { |t| t }
