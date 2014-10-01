18e634a555463d961854331a01497e87eba567ef
Varun Murali <varunm@andrew.cmu.edu>
2014-09-30 23:33:51 -0400

    display teams

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
+require 'rubygems'
+# require 'active_record'
+require 'sports_data_api'
 
 get '/' do
   @word = "Hello, World!"
+  SportsDataApi.set_key(:nfl, 'dsvqbre5qxsqkp5aemgtpgt2')
+  SportsDataApi.set_access_level(:nfl, 't')
+  @teams = SportsDataApi::Nfl.teams
+  @all_teams = @teams.map { |t| t }
+  @all_teams_names = @all_teams.map { |t| t.name }
   erb :index
 end
