c2754ff4be5c115d1da53a1cacc3784cfc74785b
Alex Egan <aegan@andrew.cmu.edu>
2014-11-10 01:35:03 -0500

    fixing master

--- helpers/team.rb
+++ helpers/team.rb
@@ -42,8 +42,8 @@ def getTeamInfo(teamName)
 	return stats
 end
 
-def getNFLTeamInfo(teamID)
+def getNHLTeamInfo(teamID)
 	SportsDataApi.set_key(:nhl, 'gk32v4v48gsfy39pqvg2hp82')
 	SportsDataApi.set_access_level(:nhl, 't')
-	
+	"we are fixing this thing"
 end
