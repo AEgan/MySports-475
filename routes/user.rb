4f5db8744b1a01e0f5aee345e361c5f934877b62
Alex Egan <aegan@andrew.cmu.edu>
2014-11-30 23:07:51 -0500

    you can sign up now

--- routes/user.rb
+++ routes/user.rb
@@ -1,3 +1,18 @@
 get '/user_tiles' do
-	
+
+end
+
+post '/signup' do
+	if request['password'] != request['password_confirmation']
+		# some error message here
+	elsif User.where(name: request['name']).entries.length != 0
+		# another error message here
+	else
+		user = User.new
+		user.name = request['name']
+		user.password = request['password']
+		user.save
+		session[:user_id] = user._id
+	end
+	redirect '/'
 end
