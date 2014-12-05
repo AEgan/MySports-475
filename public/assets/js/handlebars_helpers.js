b96011be8392110ec36032cb0a70bb40c36340da
Alex Egan <aegan@andrew.cmu.edu>
2014-12-05 00:22:42 -0500

    adds handlebar helpers to format bday and numbers over 1000

--- public/assets/js/handlebars_helpers.js
+++ public/assets/js/handlebars_helpers.js
@@ -8,6 +8,18 @@ Handlebars.registerHelper("inchesToHeight", function (inchString) {
 	return "" + feet + "'" + inches + "\"";
 });
 
+Handlebars.registerHelper("noGeekDates", function(dateString) {
+  var date = new Date(dateString);
+  return date.toDateString();
+});
+
+
+// with a little help from my friends
+// http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+Handlebars.registerHelper('numberWithCommas', function(theNumber) {
+  return theNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+});
+
 function render (tmpl, data) {
 	var div = $("#" + tmpl);
 	var html = div.html();
